948ec58397aed6fed8c2deff4bc6017f10d9eb68
Tara Z. Manicsic <tzmanics@gmail.com>
2022-02-25 22:27:38 -0500

    changes text from starter to toolbox

--- pages/success.js
+++ pages/success.js
@@ -7,13 +7,13 @@ export default function Success() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Next.js Toolbox Template!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1>Submission Received!</h1>
-        <p>Thank you for your feedback! Head back to the <Link href="/"><a>home page</a></Link></p>
+        <p>Thank you for your feedback! Head back to the <Link href="/"><a>home page</a></Link>.</p>
       </main>
 
       <Footer />
