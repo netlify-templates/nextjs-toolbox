af6ae28490c6a106495595e292fe26bc58a9027f
Prince Wilson <maxcell.wilson@gmail.com>
2022-04-21 15:34:16 -0400

    Adds project blurb to Toolbox (#28)
    * feat: adds project blurb for toolbox
    * chore: Bump next and plugin version

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import FeedbackForm from '@components/FeedbackForm'
-import JokeBlock from '@components/JokeBlock'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import FeedbackForm from "@components/FeedbackForm";
+import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
   return (
@@ -14,13 +14,15 @@ export default function Home() {
 
       <main>
         <Header title="Next.js Toolbox" />
+        <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the submissions can be found in the Netlify Admin site.
+          Here's an example of a Netlify Form! When you fill this out, the
+          submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
-        <JokeBlock/>
+        <JokeBlock />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
