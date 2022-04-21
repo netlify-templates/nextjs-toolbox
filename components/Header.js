89adea22c7d02c21f9277d49f5843a10c3ad7987
Prince Wilson <maxcell.wilson@gmail.com>
2022-04-21 11:57:33 -0400

    feat: adds project blurb for toolbox

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,26 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <>
+      <h1 className="title">{title}</h1>
+      <p>
+        Hi 👋! This template gives you a{" "}
+        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
+        <a href="https://www.netlify.com/products/functions/">
+          Netlify Functions
+        </a>
+        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
+        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
+        aim was to give you the code you would need to hit the ground running
+        with a few fun features.
+      </p>
+
+      <p>
+        You can find the code for this project on GitHub at{" "}
+        <a href="https://github.com/netlify-templates/nextjs-toolbox">
+          https://github.com/netlify-templates/nextjs-toolbox
+        </a>
+        ! Happy coding!
+      </p>
+    </>
+  );
 }
