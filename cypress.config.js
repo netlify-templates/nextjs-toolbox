af08ee2bef5b43f9c9891016ff049f274f4acca4
Tara Z. Manicsic <tzmanics@gmail.com>
2022-06-09 17:07:53 -0400

    fixes cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require('cypress')
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8888',
     supportFile: false,
